070dcf3d86f9b41954caf29c314877f4ea35c887
Will Shields <43360999+wshields@users.noreply.github.com>
2019-12-11 18:07:58 -0500

    quick reformatting

--- src/lab6/GraphMatrix.java
+++ src/lab6/GraphMatrix.java
@@ -8,7 +8,7 @@ public class GraphMatrix {
     private int[][] adjacencyMatrix;
     private int[] colorArray;
 
-    public GraphMatrix(int x){
+    public GraphMatrix(int x) {
         setLength = x;
         adjacencyMatrix = createMatrix();
         colorArray = new int[x];
@@ -32,20 +32,20 @@ public class GraphMatrix {
     }
 
     // Prints the colors array
-    public void printColors(){
+    public void printColors() {
         System.out.println(Arrays.toString(colorArray));
     }
 
-    public void computeBipartite(){
+    public void computeBipartite() {
         colorArray[0] = 1;
-        for(int i = 1; i < adjacencyMatrix[0].length; i++){
+        for (int i = 1; i < adjacencyMatrix[0].length; i++) {
             colorArray[i] = adjacencyMatrix[0][i] == 1 ? 2 : 0;
         }
-        for(int i = 1; i < colorArray.length; i++){
+        for (int i = 1; i < colorArray.length; i++) {
             int tempColor = colorArray[i];
-            for(int j = 0; j < adjacencyMatrix[i].length; j++){
-                if(adjacencyMatrix[i][j] == 1){
-                    if(colorArray[j] == tempColor){
+            for (int j = 0; j < adjacencyMatrix[i].length; j++) {
+                if (adjacencyMatrix[i][j] == 1) {
+                    if (colorArray[j] == tempColor) {
                         System.out.println("Graph is not bipartite");
                         return;
                     } else {
@@ -59,7 +59,7 @@ public class GraphMatrix {
     }
 
 
-    private int[][] createMatrix(){
+    private int[][] createMatrix() {
         int[][] ret = new int[setLength][setLength];
         Scanner key = new Scanner(System.in);
         System.out.println("Enter each row of the matrix one row at a time.");
--- src/lab6/Main.java
+++ src/lab6/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner key = new Scanner(System.in);
-	    System.out.println("Enter number of vertices in graph: ");
-	    int vertices = key.nextInt();
-	    GraphMatrix matrix = new GraphMatrix(vertices);
+        Scanner key = new Scanner(System.in);
+        System.out.println("Enter number of vertices in graph: ");
+        int vertices = key.nextInt();
+        GraphMatrix matrix = new GraphMatrix(vertices);
         matrix.printMatrix();
         matrix.computeBipartite();
         matrix.printColors();
