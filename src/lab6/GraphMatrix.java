c6a1eed3b3bd64d6e2253d85c4d3770f0d835590
Will Shields <43360999+wshields@users.noreply.github.com>
2019-12-11 17:47:24 -0500

    First commit mid algorithm creation.

---
+++ src/lab6/GraphMatrix.java
@@ -0,0 +1,72 @@
+package lab6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GraphMatrix {
+    private int setLength;
+    private int[][] adjacencyMatrix;
+    private int[] colorArray;
+
+    public GraphMatrix(int x){
+        setLength = x;
+        adjacencyMatrix = createMatrix();
+        colorArray = new int[x];
+    }
+
+    public void printMatrix() {
+        String out = "";
+        for (int i = 0; i < setLength; i++) {
+            out += "[";
+            for (int j = 0; j < this.adjacencyMatrix[i].length; j++) {
+                if (this.adjacencyMatrix[i][j] == 1) {
+                    out += j == adjacencyMatrix.length - 1 ? "1" : "1, ";
+                } else {
+                    out += j == adjacencyMatrix.length - 1 ? "0" : "0, ";
+                }
+            }
+            out = out + "] \n";
+        }
+        System.out.println(out);
+    }
+
+    public void printColors(){
+        System.out.println(Arrays.toString(colorArray));
+    }
+
+    public void computeBipartite(){
+        colorArray[0] = 1;
+        for(int i = 1; i < adjacencyMatrix[0].length; i++){
+            colorArray[i] = adjacencyMatrix[0][i] == 1 ? 2 : 0;
+        }
+    }
+
+
+    private int[][] createMatrix(){
+        int[][] ret = new int[setLength][setLength];
+        Scanner key = new Scanner(System.in);
+        System.out.println("Enter each row of the matrix one row at a time.");
+        for (int i = 0; i < setLength; i++) {
+            String temp = key.nextLine();
+            int[] toPush = new int[setLength];
+            int count = 0;
+            for (int j = 0; j < temp.length(); j++) {
+                if (temp.charAt(j) == '1') {
+                    toPush[count] = 1;
+                    count++;
+                } else if (temp.charAt(j) == '0') {
+                    toPush[count] = 0;
+                    count++;
+                } else {
+
+                }
+
+            }
+            for (int k = 0; k < toPush.length; k++) {
+                ret[i][k] = toPush[k];
+            }
+        }
+        return ret;
+    }
+
+}
---
+++ src/lab6/Main.java
@@ -0,0 +1,16 @@
+package lab6;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner key = new Scanner(System.in);
+	    System.out.println("Enter number of vertices in graph: ");
+	    int vertices = key.nextInt();
+	    GraphMatrix matrix = new GraphMatrix(vertices);
+        matrix.printMatrix();
+        matrix.computeBipartite();
+        matrix.printColors();
+    }
+}
